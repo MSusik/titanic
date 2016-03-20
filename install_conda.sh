014378df4d7a4377d4db4f5d1cf1490dcbd2524a
Mateusz Susik <mateuszsusik@gmail.com>
2016-03-20 21:43:07 +0100

    zajecia 2
    Signed-off-by: Mateusz Susik <mateuszsusik@gmail.com>

---
+++ install_conda.sh
@@ -0,0 +1,8 @@
+wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
+./Miniconda-latest-Linux-x86_64.sh
+source ~/.bashrc
+conda --version
+conda create --name knuw ipython notebook matplotlib statsmodels numpy sklearn
+source activate knuw
+ipython notebook
+
